656e22b29d50471866bd21d4a501f0af73a84b80
Jeff DeCola (TK2-PC) <jeffdecola@gmail.com>
2022-12-15 01:32:30 -0500

    update

---
+++ my-frontend-and-backend-api-examples/browser-and-web-server-apis/ajax-XHR-calls-browser-to-web-server/ajax-XHR-calls-browser-to-web-server.php
@@ -0,0 +1,14 @@
+<!-- ajax-XHR-calls-browser-to-web-server -->
+
+<link rel="stylesheet" type="text/css" href="browser-and-web-server-apis/ajax-XHR-calls-browser-to-web-server/css/ajax-XHR-calls-browser-to-web-server.css">
+
+<div id="ajax-XHR-calls-browser-to-web-server_wrapper">
+    
+    <!-- ------------------------------------------------------------------------------------------------------- -->
+    <div class="picture_container">
+
+        <img src="browser-and-web-server-apis/ajax-XHR-calls-browser-to-web-server/images/ajax-XHR-calls-browser-to-web-server.jpg" alt="ajax-XHR-calls-browser-to-web-server"/>
+
+    </div>
+
+</div>
---
+++ my-frontend-and-backend-api-examples/browser-and-web-server-apis/ajax-XHR-calls-web-server-to-browser/ajax-XHR-calls-web-server-to-browser.php
@@ -0,0 +1,14 @@
+<!-- ajax-XHR-calls-web-server-to-browser -->
+
+<link rel="stylesheet" type="text/css" href="browser-and-web-server-apis/ajax-XHR-calls-web-server-to-browser/css/ajax-XHR-calls-web-server-to-browser.css">
+
+<div id="ajax-XHR-calls-web-server-to-browser_wrapper">
+    
+    <!-- ------------------------------------------------------------------------------------------------------- -->
+    <div class="picture_container">
+
+        <img src="browser-and-web-server-apis/ajax-XHR-calls-web-server-to-browser/images/ajax-XHR-calls-web-server-to-browser.jpg" alt="ajax-XHR-calls-web-server-to-browser"/>
+
+    </div>
+
+</div>
