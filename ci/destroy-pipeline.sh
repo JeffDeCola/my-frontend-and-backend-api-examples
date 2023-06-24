95b7c6e3f835f4d1a663d72d8b99fe425d7382b0
Jeff DeCola (TK2-PC) <jeffdecola@gmail.com>
2023-06-24 14:38:10 -0400

    revamping ci and readme

--- ci/destroy-pipeline.sh
+++ ci/destroy-pipeline.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 # my-frontend-and-backend-api-examples destroy-pipeline.sh
 
-fly -t ci destroy-pipeline --pipeline my-frontend-and-backend-api-examples
+echo " "
+echo "Destroy pipeline on target jeffs-ci-target which is team jeffs-ci-team"
+fly --target jeffs-ci-target \
+    destroy-pipeline \
+    --pipeline my-frontend-and-backend-api-examples
+echo " "
